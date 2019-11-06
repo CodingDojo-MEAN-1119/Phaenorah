cb51d5cf9a23c69f10a75ec3df401f208e7c193a
phaenorah <phaenorahdemartini@yahoo.com>
2019-11-06 07:03:46 -0600

    -mongoose-dashboard

--- 04-mongodb/02-mongoose/02-mongoose-dashboard/server.js
+++ 04-mongodb/02-mongoose/02-mongoose-dashboard/server.js
@@ -30,42 +30,69 @@ mongoose.connection.once('connected', () => console.log('connected to Mongo'));
 
 const CatSchema = new mongoose.Schema({
     name: {type: String, required: true, minlength: 3},
-    age: {type: Number, required: true, minlength: 3}, 
+    age: {type: Number, required: true}, 
     favFood: {type: String, required: true, minlength: 3}, 
     spots: {type: String, required: true, minlength: 3}  
     }, {timestamps: true});  
    // create an object to that contains methods for mongoose to interface with MongoDB
 const Cat = mongoose.model('Cat', CatSchema);
 
-app.get('/', (req, res) => {  
-    res.render("index");    
+app.get('/', (req, res) => {
+    Cat.find({})
+    .then(cats => {
+        res.render("index", {cats}); 
+    })  
+});
+
+app.get('/cats/new',(req, res) => { 
+    res.render("new");
 });
 
 app.post('/cats', (req, res) => {
-    const cat = new Quote();
+    const cat = new Cat();
     cat.name = req.body.name;
-    cat.age = req.body.name;
-    cat.favFood = req.body.name;
-    cat.spots = req.body.name;
-    cat.cat = req.body.quote;
+    cat.age = req.body.age;
+    cat.favFood = req.body.favFood;
+    cat.spots = req.body.spots;
+    cat.cat = req.body.cat;
     cat.save()
     .then(savedCat => {console.log(savedCat) 
-        res.redirect('/cats')})
+        res.redirect('/')})
     .catch(err => {
         console.log("We have an error!", err);        
         for (var key in err.errors) {
             req.flash('catform', err.errors[key].message);
         }
-        res.redirect('/');
+        res.redirect('/cats/new');
     });
 })
 
-app.get('/cats', (req, res) => {  
-    Cat.find({})
-        .then(cats => {
-            console.log(cats);
-            res.render('cats', {cats: cats})})
-        .catch(err => res.json(err));
-               
+app.get('/cats/:id', (req, res) => {  
+    Cat.find({ _id: req.params.id}, function(err, response) {
+        if (err) { console.log(err); }
+        res.render('show', { cat: response[0] });
+    });               
 });
+
+app.get('/cats/edit/:id', (req, res) => {  
+    Cat.find({ _id: req.params.id}, function(err, response) {
+        if (err) { console.log(err); }
+        res.render('edit', { cat: response[0] });
+      })        
+});
+
+app.post('/:id',(req, res) => { 
+    Cat.update({_id:req.params.id}, req.body, function(err){
+        if (err) { console.log(err); }
+        res.redirect('/');
+    });
+});
+
+app.post('/cats/delete/:id',(req, res) => {
+    Cat.remove({_id: req.params.id})
+    .then(this_cat => {
+        res.redirect('/')})
+        .catch(err => res.json(err))       
+}); 
+
 app.listen(8000, () => console.log("listening on port 8000"));
