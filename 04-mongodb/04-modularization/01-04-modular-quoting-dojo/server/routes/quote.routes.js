b82156432f01504cee0162565ab55a06323a454b
phaenorah <phaenorahdemartini@yahoo.com>
2019-11-07 05:53:42 -0600

    modular-quoting-dojo

--- 04-mongodb/04-modularization/01-04-modular-quoting-dojo/server/routes/quote.routes.js
+++ 04-mongodb/04-modularization/01-04-modular-quoting-dojo/server/routes/quote.routes.js
@@ -3,5 +3,7 @@ const router = require('express').Router();
 
 module.exports = router
   .get('/', QuoteController.index)
+
   .post('/quotes', QuoteController.create)
+  
   .get('/quotes',QuoteController.show); 
