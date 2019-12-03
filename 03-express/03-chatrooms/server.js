b162af3804a23eb9a9ed4973a746104abbd85df2
phaenorah <phaenorahdemartini@yahoo.com>
2019-12-03 13:11:32 -0600

    Chatrooms

--- 03-express/03-chatrooms/server.js
+++ 03-express/03-chatrooms/server.js
@@ -3,13 +3,6 @@ const app = express();
 const server = app.listen(8000);
 const io = require('socket.io')(server);
 
-const session = require('express-session');
-app.use(session({
-  secret: 'keyboardkitteh',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}))
 
 app.use(express.static(__dirname + "/static"));
 
@@ -19,44 +12,38 @@ app.set('views', __dirname + '/views');
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req,res) => {
-  console.log("Value of name in session: ", req.session.name);
   res.render("index");
  });
-app.post('/', (req, res) => {
-  req.session.name = req.body.name;
-  res.redirect('/');
-});
 
-const users = [];
-const messages = [];
+const users = {};
+const posts = [];
 
-function isUser(user) {
-  const usersCount = users.length;
-
-  for (var i = 0; i < usersCount; i++) {
-    if (user == users[i]) {
-      return true;
-    }
+class User {
+  constructor(user_id, user_name) {
+    this.id = user_id;
+    this.name = user_name;
   }
-  return false;
+}
+class Post {
+  constructor(message, user) {
+    this.message = message;
+    this.user = user;
+  }
 }
 
 io.on('connection', function (socket) {
 
-  socket.on("got_a_new_user", function(data) {
-    const existingUser = isUser(data.user);
-      const event = existingUser ? 'existing_user' : 'load_messages';
-      const data = existingUser ? {
-                    error: "This user already exits"
-                  } : { current_user: data.user, messages: messages };
-
-      if (!existingUser) {
-        users.push(data.user);
-      }
-      socket.broadcast.emit(data, event);
-      });
-    socket.on("new_message", function(data) {
-      messages.push({ name: data.user, message: data.message });
-      socket.broadcast.emit("post_new_message", { new_message: data.message, user: data.user });
-    });
+  socket.on("got_new_user", function(data) {
+    const user = new User(socket.id, data);
+    users[user.id] = user;
+    console.log('user', user);
+    socket.emit('all_posts', posts);  //EMIT for new user
+    socket.broadcast.emit('e', posts);          //EMIT new users to existing users
+    socket.emit('all_posts', posts);  //EMIT all users to new users
+  });
+  socket.on("new_post", function({post,user}) {
+    const post = new Post(post, user);
+    posts.push(post);
+    io.emit("post_new_post", post);
+  });
 })
